5e4c73be70640e0e6c9ab25f3605ae834a977cde
404-WasFound <malachi.mcenroe@gmail.com>
2022-12-08 13:38:04 +1300

    finished day 6 part 1

---
+++ Day6/part1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::ifstream;
+
+int main(int argc, char const *argv[])
+{
+
+    // Variable declarations
+    int count = 0;
+    string packet;
+    int i;
+    int j;
+    
+    // Read line from file
+    string line;
+    ifstream File("Day6/data.txt");
+    getline(File, line);
+    
+    while (count == 0)
+    {
+
+        for (i = 0; i < line.length() - 4; i++)
+        {
+            
+            packet = "";
+
+            for (j = 0; j < 4; j++)
+            {
+                
+                if (packet.find(line[i + j]) != string::npos)
+                {
+
+                    break;
+
+                } else
+                {
+
+                    packet += line[i + j];
+
+                }
+
+            }
+
+            if (packet.length() == 4)
+            {
+
+                count = i + j;
+                break;
+
+            }
+
+        }
+
+    }
+
+    cout << count << endl;
+    cout << packet << endl;
+    
+    return 0;
+
+}
